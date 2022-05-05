39be0913e2fbd7ee704de350ecb2e92baeaa97c2
zaniu <zzaniu@126.com>
2022-05-05 20:30:40 +0800

    添加注释

--- example/client.go
+++ example/client.go
@@ -31,93 +31,94 @@ Desc   :
 package main
 
 import (
-    "context"
-    "flag"
-    "fmt"
-    "github.com/Zzaniu/zrpc"
-    "github.com/Zzaniu/zrpc/configure"
-    "github.com/Zzaniu/zrpc/configure/rpc"
-    proto2 "github.com/Zzaniu/zrpc/example/proto"
-    "github.com/Zzaniu/zrpc/tool/ztracer"
-    "google.golang.org/grpc/codes"
-    "google.golang.org/grpc/status"
-    "time"
+	"context"
+	"flag"
+	"fmt"
+	"github.com/Zzaniu/zrpc"
+	"github.com/Zzaniu/zrpc/configure"
+	"github.com/Zzaniu/zrpc/configure/rpc"
+	proto2 "github.com/Zzaniu/zrpc/example/proto"
+	"github.com/Zzaniu/zrpc/tool/ztracer"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"time"
 )
 
 type (
-    ClientConf struct {
-        *rpc.ClientConf `yaml:"ClientConf"`
-        UserServerName  string        `yaml:"UserServerName"`
-        AddServerName   string        `yaml:"AddServerName"`
-        Trace           ztracer.Trace `yaml:"Trace"`
-    }
+	ClientConf struct {
+		*rpc.ClientConf `yaml:"ClientConf"`
+		UserServerName  string        `yaml:"UserServerName"`
+		AddServerName   string        `yaml:"AddServerName"`
+		Trace           ztracer.Trace `yaml:"Trace"`
+	}
 )
 
 var clientConfigFile = flag.String("f", "clientCfg.yaml", "the config file")
 
 func main() {
-    flag.Parse()
-    cfg := ClientConf{}
-    configure.MustLoadCfg(*clientConfigFile, &cfg)
+	flag.Parse()
+	cfg := ClientConf{}
+	configure.MustLoadCfg(*clientConfigFile, &cfg)
 
-    err := ztracer.SetJaegerTracerProvider(cfg.Trace)
-    if err != nil {
-        panic(err)
-    }
+	err := ztracer.SetJaegerTracerProvider(cfg.Trace)
+	if err != nil {
+		panic(err)
+	}
 
-    // target := "discovery://172.18.2.249:20000,172.18.2.249:20002,172.18.2.249:20004/Dev/user.rpc"
-    userClient := zrpc.MustNewClient(cfg, cfg.UserServerName)
-    // 获取往 etcd 中注册的 uri
-    fmt.Println("userClient.GetTarget() = ", userClient.GetTarget())
-    addClientConn := zrpc.MustNewClientConn(cfg, cfg.AddServerName)
-    UserRpc := proto2.NewGreeterClient(userClient.Coon())
-    AddRpc := proto2.NewAddServerClient(addClientConn)
+	// target := "discovery://172.18.2.249:20000,172.18.2.249:20002,172.18.2.249:20004/Dev/user.rpc"
+	userClient := zrpc.MustNewClient(cfg, cfg.UserServerName)
+	// 获取往 etcd 中注册的 uri
+	fmt.Println("userClient.GetTarget() = ", userClient.GetTarget())
+	// 这里是为了兼容之前的
+	addClientConn := zrpc.MustNewClientConn(cfg, cfg.AddServerName)
+	UserRpc := proto2.NewGreeterClient(userClient.Coon())
+	AddRpc := proto2.NewAddServerClient(addClientConn)
 
-    for {
-        go func() {
-            ctx, cancelFunc := context.WithTimeout(context.Background(), time.Millisecond*2000)
-            deadline, ok := ctx.Deadline()
-            fmt.Println("deadline = ", deadline, ", ok = ", ok)
-            res, err := UserRpc.SayHello(ctx, &proto2.HelloRequest{Name: "小可爱"})
-            defer cancelFunc()
-            if err != nil {
-                s, ok := status.FromError(err)
-                if ok {
-                    if s.Code() == codes.DeadlineExceeded {
-                        fmt.Println("超时了, err = ", s.Message())
-                    } else if s.Code() == codes.Internal {
-                        fmt.Println("s.Message() = ", s.Message())
-                    } else if s.Code() == codes.Unavailable {
-                        fmt.Println("熔断了")
-                    }
-                } else {
-                    panic(err)
-                }
-            } else {
-                fmt.Println("res.Message = ", res.Message)
-            }
-        }()
+	for {
+		go func() {
+			ctx, cancelFunc := context.WithTimeout(context.Background(), time.Millisecond*2000)
+			deadline, ok := ctx.Deadline()
+			fmt.Println("deadline = ", deadline, ", ok = ", ok)
+			res, err := UserRpc.SayHello(ctx, &proto2.HelloRequest{Name: "小可爱"})
+			defer cancelFunc()
+			if err != nil {
+				s, ok := status.FromError(err)
+				if ok {
+					if s.Code() == codes.DeadlineExceeded {
+						fmt.Println("超时了, err = ", s.Message())
+					} else if s.Code() == codes.Internal {
+						fmt.Println("s.Message() = ", s.Message())
+					} else if s.Code() == codes.Unavailable {
+						fmt.Println("熔断了")
+					}
+				} else {
+					panic(err)
+				}
+			} else {
+				fmt.Println("res.Message = ", res.Message)
+			}
+		}()
 
-        go func() {
-            res2, err := AddRpc.AddInt(context.Background(), &proto2.AddIntRequest{Value2: 2, Value1: 1})
+		go func() {
+			res2, err := AddRpc.AddInt(context.Background(), &proto2.AddIntRequest{Value2: 2, Value1: 1})
 
-            if err != nil {
-                s, ok := status.FromError(err)
-                if ok {
-                    if s.Code() == codes.DeadlineExceeded {
-                        fmt.Println("超时了, err = ", s.Message())
-                    } else if s.Code() == codes.Internal {
-                        fmt.Println("s.Message() = ", s.Message())
-                    }
-                } else {
-                    panic(err)
-                }
-            } else {
-                fmt.Println("res2.Message = ", res2.Message)
-            }
-        }()
-        // time.Sleep(time.Millisecond * 5000)
-        break
-    }
-    time.Sleep(time.Second * 5)
+			if err != nil {
+				s, ok := status.FromError(err)
+				if ok {
+					if s.Code() == codes.DeadlineExceeded {
+						fmt.Println("超时了, err = ", s.Message())
+					} else if s.Code() == codes.Internal {
+						fmt.Println("s.Message() = ", s.Message())
+					}
+				} else {
+					panic(err)
+				}
+			} else {
+				fmt.Println("res2.Message = ", res2.Message)
+			}
+		}()
+		// time.Sleep(time.Millisecond * 5000)
+		break
+	}
+	time.Sleep(time.Second * 5)
 }
