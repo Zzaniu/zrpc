f891deae6ec8a507503cf3ac09eb5939276d61ca
Zzaniu <zzaniu@126.com>
2022-07-19 18:17:45 +0800

    优化超时时间

--- middleware/timeout/timeout.go
+++ middleware/timeout/timeout.go
@@ -40,12 +40,12 @@ import (
 func TimeoutInterceptor(timeout time.Duration) grpc.UnaryClientInterceptor {
     return func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn,
         invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
-        if timeout <= 0 {
-            return invoker(ctx, method, req, reply, cc, opts...)
+        // 如果当前没有设置超时时间且默认超时时间大于0, 则给个默认的超时时间. 以当前传入的超时时间为主
+        if _, ok := ctx.Deadline(); !ok && timeout > 0 {
+            ctx2, cancel := context.WithTimeout(ctx, timeout)
+            defer cancel()
+            ctx = ctx2
         }
-
-        ctx, cancel := context.WithTimeout(ctx, timeout)
-        defer cancel()
         // grpc 已经实现了超时控制，所以这里只要把ctx替换成带超时的ctx就行了
         return invoker(ctx, method, req, reply, cc, opts...)
     }
